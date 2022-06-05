0309577db2e18fc4da2cc8084b84d9c4d2211b3a
liou <liou666@126.con>
2022-06-05 23:12:55 +0800

    chore: init

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from '~/root/App';
 
-(window as any).meta={}
 ReactDOM.render(
   <React.StrictMode>
-    {/* {Object.keys((window as any).meta).length&&(window as any).meta} */}
     <App />
-   
   </React.StrictMode>,
   document.getElementById('root')
 );
